1b1ce8617f17a5d2deabaab0b161526ee5c769a3
Alexander McLean <alexandermclean@Alexanders-iMac.local>
2021-10-10 07:27:05 +0100

    added code to dynamically add the pizza and topping pages

---
+++ web/src/templates/Toppings.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PizzaList from "../components/PizzaList";
+import ToppingsFilter from "../components/ToppingsFilter";
+
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
+  return (
+    <>
+      <ToppingsFilter />
+      <PizzaList pizzas={pizzas} />
+    </>
+  );
+}
+
+// TODO
+// change query to only select the pizzas with the correct toppings
+
+export const query = graphql`
+  query($slug: String!) {
+    pizza: sanityPizza(slug: { current: { eq: $slug } }) {
+      name
+      id
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      toppings {
+        name
+        id
+        vegetarian
+      }
+    }
+  }
+`;
