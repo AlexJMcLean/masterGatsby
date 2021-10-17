90c7afdd1997d5674a25ececc02fb9074d26c498
Alexander McLean <alexandermclean@Alexanders-iMac.local>
2021-10-17 08:22:56 +0100

    Finished styling for slicemasters

--- web/src/components/SlicemasterList.js
+++ web/src/components/SlicemasterList.js
@@ -1,25 +1,50 @@
 import React from "react";
 import styled from "styled-components";
 import Img from "gatsby-image";
+import { Link } from "gatsby";
 
 const SlicemasterGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 4rem;
 `;
 
 const SlicemasterStyles = styled.div`
-  background-color: black;
+  a {
+    text-decoration: none;
+  }
+  h2 {
+    font-size: 4rem;
+    transform: rotate(-2deg);
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 1;
+    text-align: center;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  p {
+    width: 80%;
+    text-align: center;
+    position: relative;
+    margin: -2rem auto 0;
+    background-color: var(--yellow);
+    z-index: 1;
+    transform: rotate(2deg);
+  }
 `;
 
 function SingleSlicemaster({ person }) {
   return (
     <SlicemasterStyles key={person.id}>
-      <h2>
-        <span class="mark">{person.name}</span>
-      </h2>
-      <Img fluid={person.image.asset.fluid} alt={person.name} />
-      <p>{person.description}</p>
+      <Link to={`/slicemaster/${person.name}`}>
+        <h2>
+          <span class="mark">{person.name}</span>
+        </h2>
+        <Img fluid={person.image.asset.fluid} alt={person.name} />
+        <p>{person.description}</p>
+      </Link>
     </SlicemasterStyles>
   );
 }
